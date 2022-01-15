f0deb0727ebe14d5083a3f208d715ea7665a2426
farzad <farzadafi40@gmail.com>
2022-01-15 19:04:46 +0330

    add singleton connection to bankRepository

--- Bank/src/main/java/Repository/BankRepository.java
+++ Bank/src/main/java/Repository/BankRepository.java
@@ -1,4 +1,12 @@
 package Repository;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class BankRepository {
+    private Connection connection = Singleton.getInstance().getConnection();
+
+    public BankRepository() throws SQLException, ClassNotFoundException {
+
+    }
 }
