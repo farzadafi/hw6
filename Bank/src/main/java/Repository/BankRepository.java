626f1db42ec012243734b8f385a96b3111274b9d
farzad <farzadafi40@gmail.com>
2022-01-15 19:53:26 +0330

    add findBank method to BankRepository

--- Bank/src/main/java/Repository/BankRepository.java
+++ Bank/src/main/java/Repository/BankRepository.java
@@ -1,7 +1,10 @@
 package Repository;
 
+import Entity.Bank;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BankRepository {
@@ -11,5 +14,16 @@ public class BankRepository {
         String createTableBank = "CREATE TABLE IF NOT EXISTS Bank (id serial,name varchar(50)) ";
         PreparedStatement preparedStatement = connection.prepareStatement(createTableBank);
         preparedStatement.execute();
+    }
+
+    public int findBank(Bank bank) throws SQLException {
+        String find = "SELECT * FROM Bank WHERE name = ? ";
+        PreparedStatement preparedStatement = connection.prepareStatement(find);
+        preparedStatement.setString(1,bank.getName());
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next())
+            return 1;
+        else
+            return 0;
     }
 }
