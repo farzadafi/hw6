b46a06539dcc5f2e121b3454bd2c22608792a12b
farzad <farzadafi40@gmail.com>
2022-01-15 19:08:13 +0330

    add constructor for createTableBank

--- Bank/src/main/java/Repository/BankRepository.java
+++ Bank/src/main/java/Repository/BankRepository.java
@@ -1,12 +1,15 @@
 package Repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class BankRepository {
     private Connection connection = Singleton.getInstance().getConnection();
 
     public BankRepository() throws SQLException, ClassNotFoundException {
-
+        String createTableBank = "CREATE TABLE IF NOT EXISTS Bank (id serial,name varchar(50)) ";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTableBank);
+        preparedStatement.execute();
     }
 }
