65e92056b6ab6aa0e3bd2df7ddc6247cd264eb66
farzad <farzadafi40@gmail.com>
2022-01-15 20:20:20 +0330

    add while to main method!

--- Bank/src/main/java/App.java
+++ Bank/src/main/java/App.java
@@ -4,6 +4,31 @@
 public class App {
     public static void main(String[] args) {
 
+        Menu menu = new Menu();
 
+        while(true){
+            switch(menu.publicMenu()){
+                case 1:
+                    menu.RigesterMenu();
+                    break;
+
+                case 2:
+                    menu.EnterMenu();
+                    break;
+
+                case 3:
+                    System.out.println("Have a nice day!");
+                    break;
+
+                case 0:
+                    System.out.println("you enter a wrong number!");
+            }
+        }
     }
+
+
+
+
+
+
 }
