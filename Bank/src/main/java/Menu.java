91c6779c27aeb58fe042e377bb0bea86b94202e8
farzad <farzadafi40@gmail.com>
2022-01-15 20:16:12 +0330

    add publicMenu to menuCalss

--- Bank/src/main/java/Menu.java
+++ Bank/src/main/java/Menu.java
@@ -1,2 +1,61 @@
+import java.util.Scanner;
+
 public class Menu {
+    Scanner input = new Scanner(System.in);
+    int command;
+
+    public int publicMenu(){
+        System.out.println("***WELCOME***");
+        System.out.println("1-signUp(Register)");
+        System.out.println("2-signIn(Enter)");
+        System.out.println("3-Exit");
+        System.out.print("Please select a number:");
+        command = input.nextInt();
+        input.nextLine();
+        switch(command){
+            case 1:
+                return 1;
+
+            case 2:
+                return 2;
+
+            case 3:
+                return 3;
+
+            default:
+                return 0;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
