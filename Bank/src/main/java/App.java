2e101ab8aaef322589d322484d578bfb0d0e34b0
farzad <farzadafi40@gmail.com>
2022-01-16 06:42:12 +0330

    fix throws exception in main

--- Bank/src/main/java/App.java
+++ Bank/src/main/java/App.java
@@ -1,8 +1,7 @@
-
-
+import java.sql.SQLException;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         Menu menu = new Menu();
 
