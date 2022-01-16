6f42753c26fb688e2434c5017a04420d4e20ac1c
farzad <farzadafi40@gmail.com>
2022-01-16 15:08:10 +0330

    add constructor for create table and add method for insert to table in ClerkRepository

--- Bank/src/main/java/Repository/ClerkRepository.java
+++ Bank/src/main/java/Repository/ClerkRepository.java
@@ -2,13 +2,33 @@ package Repository;
 
 import Entity.Clerk;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ClerkRepository implements Repository<Clerk> {
+    private Connection connection = Singleton.getInstance().getConnection();
+
+    public ClerkRepository() throws SQLException, ClassNotFoundException {
+        String createTable ="CREATE TABLE IF NOT EXISTS " +
+                            "Clerk(id serial," +
+                            "fullName varchar(50)," +
+                            "nationalId varchar(50) PRIMARY KEY ," +
+                            "codeBranch varchar(50)," + "" +
+                            "password varchar(50))";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+        preparedStatement.execute();
+    }
 
     @Override
     public void add(Clerk clerk) throws SQLException {
-
+        String insert = " INSERT INTO Clerk(fullName,nationalId,codeBranch,password) VALUES (?, ?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(insert);
+        preparedStatement.setString(1,clerk.getFullName());
+        preparedStatement.setString(2,clerk.getNationalId());
+        preparedStatement.setString(3,clerk.getCodeBranch());
+        preparedStatement.setString(4,clerk.getPassword());
+        preparedStatement.executeUpdate();
     }
 
     @Override
