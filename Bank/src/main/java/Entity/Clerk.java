67ec3082e59051ed1fb8512c0ec49fd82d31d67c
farzad <farzadafi40@gmail.com>
2022-01-16 14:43:45 +0330

    create clerk class and create constructor fot it

---
+++ Bank/src/main/java/Entity/Clerk.java
@@ -0,0 +1,9 @@
+package Entity;
+
+public class Clerk extends Person{
+
+    public Clerk(String fullName, String nationalId, String codeBranch, String password, TypeUser typeUser) {
+        super(fullName, nationalId, codeBranch, password, typeUser);
+    }
+
+}
