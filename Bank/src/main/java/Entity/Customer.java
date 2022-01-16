7c3a2b729443882d8851aab1a0e38da9cd87478b
farzad <farzadafi40@gmail.com>
2022-01-16 16:35:31 +0330

    create Customer clss

---
+++ Bank/src/main/java/Entity/Customer.java
@@ -0,0 +1,8 @@
+package Entity;
+
+public class Customer extends Person{
+
+    public Customer(String fullName, String nationalId, String codeBranch, String password, TypeUser typeUser) {
+        super(fullName, nationalId, codeBranch, password, typeUser);
+    }
+}
