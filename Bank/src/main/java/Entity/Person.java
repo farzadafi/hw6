4133f9180a73efb0ad49b3b4d7080701d0e4a1b2
farzad <farzadafi40@gmail.com>
2022-01-16 14:43:15 +0330

    add constructor to person class

--- Bank/src/main/java/Entity/Person.java
+++ Bank/src/main/java/Entity/Person.java
@@ -4,5 +4,11 @@ public class Person {
     private String fullName,nationalId,codeBranch,password;
     TypeUser typeUser;
 
-
+    public Person(String fullName, String nationalId, String codeBranch, String password, TypeUser typeUser) {
+        this.fullName = fullName;
+        this.nationalId = nationalId;
+        this.codeBranch = codeBranch;
+        this.password = password;
+        this.typeUser = typeUser;
+    }
 }
