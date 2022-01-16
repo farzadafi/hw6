26df67fe89b8972e0b1eb988a8f328f8146a887f
farzad <farzadafi40@gmail.com>
2022-01-16 17:06:27 +0330

    add addCustomer method to CustomerService

--- Bank/src/main/java/Service/CustomerService.java
+++ Bank/src/main/java/Service/CustomerService.java
@@ -1,4 +1,45 @@
 package Service;
 
+import Entity.Clerk;
+import Entity.Customer;
+import Entity.TypeUser;
+import Repository.CustomerRepository;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class CustomerService {
+    private Scanner input = new Scanner(System.in);
+    private String fullName,nationalId,codeBranch,password;
+    private ClerkService clerkService = new ClerkService();
+    private LoginService loginService = new LoginService();
+    private CustomerRepository customerRepository = new CustomerRepository();
+
+    public CustomerService() throws SQLException, ClassNotFoundException {
+    }
+
+
+    //::::>
+    public String addCustomer(String nationalIdClerk) throws SQLException {
+        codeBranch = clerkService.findCodeBranch(nationalIdClerk);
+        System.out.print("Enter full Name customer:");
+        fullName = input.nextLine();
+        while(true){
+            System.out.print("Enter nationalId(username):");
+            nationalId = input.nextLine();
+            if( loginService.findNationalId(nationalId) == 1 ) {
+                System.out.println("You enter a wrong national Id!");
+            }
+            else
+                break;
+        }
+        System.out.print("Enter password for " + fullName + " :");
+        password = input.nextLine();
+        Customer newClerk = new Customer(fullName,nationalId,codeBranch,password, TypeUser.CUSTOMER);
+        customerRepository.add(newClerk);
+        loginService.addNewLogin(nationalId,password,TypeUser.CUSTOMER);
+        return fullName;
+    }
+
+
 }
