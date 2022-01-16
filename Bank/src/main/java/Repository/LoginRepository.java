2cd1538531cf55387a87db12a45fc4876bfecefe
farzad <farzadafi40@gmail.com>
2022-01-16 08:32:20 +0330

    add find method to Login Repository

--- Bank/src/main/java/Repository/LoginRepository.java
+++ Bank/src/main/java/Repository/LoginRepository.java
@@ -1,10 +1,13 @@
 package Repository;
 
+import Entity.Login;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class LoginRepository {
+public class LoginRepository implements Repository<Login> {
     private Connection connection = Singleton.getInstance().getConnection();
 
     public LoginRepository() throws SQLException, ClassNotFoundException {
@@ -12,4 +15,22 @@ public class LoginRepository {
         PreparedStatement preparedStatement = connection.prepareStatement(createTable);
         preparedStatement.execute();
     }
+
+    @Override
+    public void add(Login login) throws SQLException {
+
+    }
+
+    @Override
+    public int find(String input) throws SQLException {
+        String find = "SELECT * FROM Login WHERE username = ? ";
+        PreparedStatement preparedStatement = connection.prepareStatement(find);
+        preparedStatement.setString(1,input);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next())
+            return 1;
+        else
+            return 0;
+    }
+
 }
