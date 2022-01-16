fa488e74f8dd6db84bdd3f978a5f2d41500c9611
farzad <farzadafi40@gmail.com>
2022-01-16 18:45:45 +0330

    add addAccount method to AccountService

--- Bank/src/main/java/Service/AccountService.java
+++ Bank/src/main/java/Service/AccountService.java
@@ -1,4 +1,52 @@
 package Service;
 
+import Entity.Account;
+import Entity.Customer;
+import Entity.TypeAccount;
+import Entity.TypeUser;
+import Repository.AccountRepository;
+
+import java.sql.SQLException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class AccountService {
+    private Scanner input = new Scanner(System.in);
+    private ClerkService clerkService = new ClerkService();
+    private LoginService loginService = new LoginService();
+    private String codeBranch,nationalId,accountNumber;
+    private AccountRepository accountRepository = new AccountRepository();
+    private Double budget;
+    Random random = new Random();
+
+    public AccountService() throws SQLException, ClassNotFoundException {
+    }
+
+    //::::>
+    public int addAccount(String nationalIdClerk) throws SQLException {
+        codeBranch = clerkService.findCodeBranch(nationalIdClerk);
+        while(true){
+            System.out.print("Enter nationalId(username):");
+            nationalId = input.nextLine();
+            if( loginService.findNationalId(nationalId) == 0 ) {
+                System.out.println("You enter a wrong national Id!");
+            }
+            else
+                break;
+        }
+        int number = random.nextInt(8);
+        System.out.print("Enter budget:");
+        budget = input.nextDouble();
+        Account newAccount = new Account(codeBranch,nationalId,String.valueOf(number),budget, TypeAccount.ACTIVE);
+        accountRepository.add(newAccount);
+        return 1;
+    }
+
+
+
+
+
+
+
+
 }
