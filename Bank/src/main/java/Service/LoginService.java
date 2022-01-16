507bea00e4f81b1efec191baae76cbd786cc2fdc
farzad <farzadafi40@gmail.com>
2022-01-16 08:36:54 +0330

    add findNationalId to LoginService class

--- Bank/src/main/java/Service/LoginService.java
+++ Bank/src/main/java/Service/LoginService.java
@@ -1,4 +1,19 @@
 package Service;
 
+import Repository.LoginRepository;
+
+import java.sql.SQLException;
+
 public class LoginService {
-}
+    private LoginRepository loginRepository = new LoginRepository();
+
+    public LoginService() throws SQLException, ClassNotFoundException {
+    }
+
+    //::::>
+    public int findNationalId(String nationalId) throws SQLException {
+        return loginRepository.find(nationalId);
+    }
+
+
+}//
