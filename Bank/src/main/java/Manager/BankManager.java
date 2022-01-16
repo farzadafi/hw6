baf59cfcbd600035082497deeb970649e339606f
farzad <farzadafi40@gmail.com>
2022-01-16 06:41:32 +0330

    add addBank to BankManager

--- Bank/src/main/java/Manager/BankManager.java
+++ Bank/src/main/java/Manager/BankManager.java
@@ -1,4 +1,20 @@
 package Manager;
 
+import Entity.Bank;
+import Service.BankService;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class BankManager {
+    private Scanner input = new Scanner(System.in);
+    private BankService bankService;
+    private String name;
+
+    public int addBank() throws SQLException {
+        System.out.print("Please enter name of Bank for add:");
+        name = input.nextLine();
+        Bank newBank = new Bank(name);
+        return bankService.addBank(newBank);
+    }
 }
