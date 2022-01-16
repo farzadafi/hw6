227cb1e49ff4b40f8c665f1fa9aa7d61ef4b9ed9
farzad <farzadafi40@gmail.com>
2022-01-16 06:10:09 +0330

    fix Repository class

--- Bank/src/main/java/Repository/Repository.java
+++ Bank/src/main/java/Repository/Repository.java
@@ -1,4 +1,10 @@
 package Repository;
 
-public interface Repository {
+import Entity.Bank;
+
+import java.sql.SQLException;
+
+public interface Repository<T> {
+    public void add(T t)throws SQLException;
+
 }
