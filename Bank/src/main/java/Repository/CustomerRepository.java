7e1c8ffd075bfb552761a92f1395c0d99130d4fc
farzad <farzadafi40@gmail.com>
2022-01-16 16:47:15 +0330

    add constructor for create Table in CustomerRepository

--- Bank/src/main/java/Repository/CustomerRepository.java
+++ Bank/src/main/java/Repository/CustomerRepository.java
@@ -2,9 +2,24 @@ package Repository;
 
 import Entity.Customer;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CustomerRepository implements Repository<Customer> {
+    Connection connection = Singleton.getInstance().getConnection();
+
+    public CustomerRepository() throws SQLException, ClassNotFoundException {
+        String createTable ="CREATE TABLE IF NOT EXISTS " +
+                "Customer(id serial," +
+                "fullName varchar(50)," +
+                "nationalId varchar(50) PRIMARY KEY ," +
+                "codeBranch varchar(50)," + "" +
+                "password varchar(50))";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+        preparedStatement.execute();
+    }
+
     @Override
     public void add(Customer customer) throws SQLException {
 
