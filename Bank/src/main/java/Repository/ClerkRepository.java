efa3318130a2e8751de527d59b4a037851ae66b8
farzad <farzadafi40@gmail.com>
2022-01-16 14:53:07 +0330

    create ClerkRepository class

---
+++ Bank/src/main/java/Repository/ClerkRepository.java
@@ -0,0 +1,18 @@
+package Repository;
+
+import Entity.Clerk;
+
+import java.sql.SQLException;
+
+public class ClerkRepository implements Repository<Clerk> {
+
+    @Override
+    public void add(Clerk clerk) throws SQLException {
+
+    }
+
+    @Override
+    public int find(String input) throws SQLException {
+        return 0;
+    }
+}
