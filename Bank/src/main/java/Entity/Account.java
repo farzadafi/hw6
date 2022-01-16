5181177db59d04e186e004d7ef74698141c9b8d4
farzad <farzadafi40@gmail.com>
2022-01-16 18:13:56 +0330

    add constructor and all getter to Account calss

--- Bank/src/main/java/Entity/Account.java
+++ Bank/src/main/java/Entity/Account.java
@@ -1,4 +1,30 @@
 package Entity;
+import java.math.BigInteger;
 
 public class Account {
+    private String codeBranck,nationalId,accountNumber;
+    BigInteger budget;
+
+    public Account(String codeBranck, String nationalId, String accountNumber, BigInteger budget) {
+        this.codeBranck = codeBranck;
+        this.nationalId = nationalId;
+        this.accountNumber = accountNumber;
+        this.budget = budget;
+    }
+
+    public String getCodeBranck() {
+        return codeBranck;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public BigInteger getBudget() {
+        return budget;
+    }
 }
