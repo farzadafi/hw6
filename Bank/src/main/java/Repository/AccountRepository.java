eef152cf81621dd4ae29ecc889a1a5923bd5b23f
farzad <farzadafi40@gmail.com>
2022-01-16 18:26:58 +0330

    add constructor to AccountRepository for create Table

--- Bank/src/main/java/Repository/AccountRepository.java
+++ Bank/src/main/java/Repository/AccountRepository.java
@@ -2,9 +2,25 @@ package Repository;
 
 import Entity.Account;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AccountRepository implements Repository<Account> {
+    private Connection connection = Singleton.getInstance().getConnection();;
+
+    //::::>
+    public AccountRepository() throws SQLException, ClassNotFoundException {
+        String createTable ="CREATE TABLE IF NOT EXISTS " +
+                "Account(id serial," +
+                "codeBranch varchar(50) REFERENCES BankBranch(codeBranch)," +
+                "nationalId varchar(50) REFERENCES Customer(nationalId)," +
+                "accountNumber varchar(50) ," +
+                "budget bigint, " +
+                "status varchar(50))";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+        preparedStatement.execute();
+    }
 
     @Override
     public void add(Account account) throws SQLException {
