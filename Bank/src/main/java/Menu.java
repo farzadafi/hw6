2773a0d474e1f859b3eec3c1116407e9412211c1
farzad <farzadafi40@gmail.com>
2022-01-16 05:59:42 +0330

    add enterMenu to menu class

--- Bank/src/main/java/Menu.java
+++ Bank/src/main/java/Menu.java
@@ -3,28 +3,37 @@ import java.util.Scanner;
 public class Menu {
     Scanner input = new Scanner(System.in);
     int command;
+    String username,password;
 
+    //::::>
     public int publicMenu(){
         System.out.println("***WELCOME***");
-        System.out.println("1-signUp(Register)");
-        System.out.println("2-signIn(Enter)");
-        System.out.println("3-Exit");
+        System.out.println("1-signIn(Enter)");
+        System.out.println("2-Exit");
         System.out.print("Please select a number:");
         command = input.nextInt();
         input.nextLine();
         switch(command){
             case 1:
-                return 1;
-
-            case 2:
                 return 2;
 
-            case 3:
+            case 2:
                 return 3;
 
             default:
                 return 0;
         }
+    }
+
+    //::::>
+    public void enterMenu(){
+        System.out.print("Please enter your username:");
+        username = input.nextLine();
+        System.out.print("Please enter your password:");
+        password = input.nextLine();
+        if( username.equals("admin") && password.equals("admin") )
+            adminMenu();
+
     }
 
 
