1713521f3c4a060ef9a7836e989bb70743715685
farzad <farzadafi40@gmail.com>
2022-01-16 16:49:47 +0330

    add all getter in Customer class

--- Bank/src/main/java/Entity/Customer.java
+++ Bank/src/main/java/Entity/Customer.java
@@ -5,4 +5,20 @@ public class Customer extends Person{
     public Customer(String fullName, String nationalId, String codeBranch, String password, TypeUser typeUser) {
         super(fullName, nationalId, codeBranch, password, typeUser);
     }
+
+    public String getFullName(){
+        return super.getFullName();
+    }
+
+    public String getNationalId(){
+        return super.getNationalId();
+    }
+
+    public String getCodeBranch(){
+        return super.getCodeBranch();
+    }
+
+    public String getPassword(){
+        return super.getPassword();
+    }
 }
