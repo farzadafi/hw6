05874573c38f5cff3551f890a925bde494ec304a
farzad <farzadafi40@gmail.com>
2022-01-16 08:28:25 +0330

    add LoginRepository(constructor) to LoginRepository

--- Bank/src/main/java/Repository/LoginRepository.java
+++ Bank/src/main/java/Repository/LoginRepository.java
@@ -1,4 +1,15 @@
 package Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class LoginRepository {
+    private Connection connection = Singleton.getInstance().getConnection();
+
+    public LoginRepository() throws SQLException, ClassNotFoundException {
+        String createTable = "CREATE TABLE IF NOT EXISTS Login(id serial,username varchar(50) PRIMARY KEY,password varchar(50),kind varchar(50) )";
+        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+        preparedStatement.execute();
+    }
 }
