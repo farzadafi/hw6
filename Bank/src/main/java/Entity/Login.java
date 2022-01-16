769e24f9156dda7b01a6e22771caddeeef760981
farzad <farzadafi40@gmail.com>
2022-01-16 07:18:42 +0330

    add Login class Entity

---
+++ Bank/src/main/java/Entity/Login.java
@@ -0,0 +1,27 @@
+package Entity;
+
+public class Login {
+    private String username;
+    private String password;
+    private TypeUser typeUser;
+
+    public Login(){}
+
+    public Login(String username, String password, TypeUser typeUser) {
+        this.username = username;
+        this.password = password;
+        this.typeUser = typeUser;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public TypeUser getTypeUser() {
+        return typeUser;
+    }
+}
