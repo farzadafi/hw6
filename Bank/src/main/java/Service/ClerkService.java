05cd3e8b1bb28cdb5ab01db253c556a7dcc42b76
farzad <farzadafi40@gmail.com>
2022-01-16 15:41:13 +0330

    fix addClerk in ClerkService

--- Bank/src/main/java/Service/ClerkService.java
+++ Bank/src/main/java/Service/ClerkService.java
@@ -2,24 +2,23 @@ package Service;
 
 import Entity.Clerk;
 import Entity.TypeUser;
-import Repository.BankBranchRepository;
 import Repository.ClerkRepository;
 
 import java.sql.SQLException;
 import java.util.Scanner;
 
-class ClerkService {
+public class ClerkService {
     Scanner input = new Scanner(System.in);
     private String fullName,nationalId,codeBranch,password;
     ClerkRepository clerkRepository = new ClerkRepository();
     BankBranchService bankBranchService = new BankBranchService();
     LoginService loginService = new LoginService();
 
-    ClerkService() throws SQLException, ClassNotFoundException {
+    public ClerkService() throws SQLException, ClassNotFoundException {
     }
 
     //::::>
-    public void addClerk(String nationalIdBoss) throws SQLException {
+    public String addClerk(String nationalIdBoss) throws SQLException {
         codeBranch = bankBranchService.findCodeBranch(nationalIdBoss);
         System.out.print("Enter full Name Clerk:");
         fullName = input.nextLine();
@@ -37,6 +36,7 @@ class ClerkService {
         Clerk newClerk = new Clerk(fullName,nationalId,codeBranch,password,TypeUser.CLERK);
         clerkRepository.add(newClerk);
         loginService.addNewLogin(nationalId,password,TypeUser.CLERK);
+        return fullName;
     }
 
 
