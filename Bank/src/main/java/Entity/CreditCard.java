4a80306887d57ba619a52aa82e7a714664bf2e69
farzad <farzadafi40@gmail.com>
2022-01-17 06:47:33 +0330

    add constructor and all getter to CreditCard class

--- Bank/src/main/java/Entity/CreditCard.java
+++ Bank/src/main/java/Entity/CreditCard.java
@@ -1,4 +1,44 @@
 package Entity;
 
+import java.sql.Date;
+
 public class CreditCard {
+    private String accountNumber,cardNumber,cvv2,password;
+    private Date date;
+    private TypeAccount typeAccount;
+
+    public CreditCard(String accountNumber, String cardNumber, String cvv2, String password, Date date, TypeAccount typeAccount) {
+        this.accountNumber = accountNumber;
+        this.cardNumber = cardNumber;
+        this.cvv2 = cvv2;
+        this.password = password;
+        this.date = date;
+        this.typeAccount = typeAccount;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getCvv2() {
+        return cvv2;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public TypeAccount getTypeAccount() {
+        return typeAccount;
+    }
+
+
 }
