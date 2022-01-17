eca82bc99a9f280e9b64f30346d31b6397092e49
farzad <farzadafi40@gmail.com>
2022-01-17 10:14:26 +0330

    add addCard method to CreditCardService

--- Bank/src/main/java/Service/CreditCardService.java
+++ Bank/src/main/java/Service/CreditCardService.java
@@ -1,4 +1,72 @@
 package Service;
 
+import Entity.Account;
+import Entity.CreditCard;
+import Entity.TypeAccount;
+import Repository.CreditCardRepository;
+
+import java.sql.SQLException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class CreditCardService {
+    private AccountService accountService = new AccountService();
+    private Random random = new Random();
+    private CustomerService customerService = new CustomerService();
+    private String nationalId,name = "empty",accountNumber,cardNumber,cvv2,expireDate;
+    private Scanner input = new Scanner(System.in);
+    private LoginService loginService = new LoginService();
+    private CreditCardRepository creditCardRepository = new CreditCardRepository();
+
+    public CreditCardService() throws SQLException, ClassNotFoundException {
+    }
+
+    public int addCard() throws SQLException {
+        while(true){
+            System.out.print("Enter national Id customer(cancel):");
+            nationalId = input.nextLine();
+            if( nationalId.equals("cancel"))
+                return 0;
+            if( loginService.findNationalId(nationalId) == 0 ) {
+                System.out.println("You enter a wrong national Id!");
+            }
+            else
+                break;
+        }
+        name = customerService.findName(nationalId);
+        if ( name.equals("null")){
+            System.out.println("you enter a wrong national id!");
+            return 0;
+        }
+        System.out.println( name + " have this account :");
+        int result = accountService.showAccount(nationalId);
+        if( result == 0 ){
+            System.out.println(name + " unfortunately doesn't have any active account!");
+            return 0;
+        }
+        System.out.print("Enter account number for add card it:");
+        accountNumber = input.nextLine();
+        if ( accountService.findAccountNumber(accountNumber) == 0 ){
+            System.out.println("this account number is not define!");
+            return 0;
+        }
+
+        while(true){
+            cardNumber = String.valueOf(random.nextLong(111111111111L,999999999999L));
+            if( creditCardRepository.find(cardNumber) == 0 )
+                break;
+        }
+        cvv2 = String.valueOf(random.nextInt(1111,9999));
+        int year = java.time.LocalDate.now().getYear() + 4 ;
+        int month = java.time.LocalDate.now().getMonthValue();
+        expireDate = (String.valueOf(year)) + "-" + (String.valueOf(month));
+        CreditCard newCreditCard = new CreditCard(accountNumber,cardNumber,cvv2,expireDate,TypeAccount.ACTIVE);
+        creditCardRepository.add(newCreditCard);
+        return 1;
+    }
+
+
+
+
+
 }
