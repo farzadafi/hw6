c10fe25f150454875e433def2415a891a9bbe5f7
farzad <farzadafi40@gmail.com>
2022-01-18 14:46:39 +0330

    add addTransaction method to TransactionService

--- Bank/src/main/java/Service/TransactionService.java
+++ Bank/src/main/java/Service/TransactionService.java
@@ -1,4 +1,17 @@
 package Service;
 
+import Entity.Transaction;
+import Repository.TransactionRepository;
+
+import java.sql.SQLException;
+
 public class TransactionService {
+    private TransactionRepository transactionRepository = new TransactionRepository();
+
+    public TransactionService() throws SQLException, ClassNotFoundException {
+    }
+
+    public void addTransaction(Transaction transaction) throws SQLException {
+        transactionRepository.add(transaction);
+    }
 }
