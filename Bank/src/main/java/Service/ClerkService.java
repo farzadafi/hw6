420d5c86dc7e5dbe9bb925a40b8ee977c4c45396
farzad <farzadafi40@gmail.com>
2022-01-26 14:13:57 +0330

    fix addClerk in ClerkService(add try catch for give correct input)

--- Bank/src/main/java/Service/ClerkService.java
+++ Bank/src/main/java/Service/ClerkService.java
@@ -13,6 +13,7 @@ public class ClerkService {
     ClerkRepository clerkRepository = new ClerkRepository();
     BankBranchService bankBranchService = new BankBranchService();
     LoginService loginService = new LoginService();
+    private Utility utility = new Utility();
 
     public ClerkService() throws SQLException, ClassNotFoundException {
     }
@@ -20,19 +21,19 @@ public class ClerkService {
     //::::>
     public String addClerk(String nationalIdBoss) throws SQLException {
         codeBranch = bankBranchService.findCodeBranch(nationalIdBoss);
-        System.out.print("Enter full Name Clerk:");
-        fullName = input.nextLine();
+        System.out.println("Enter full Name Clerk");
+        fullName = utility.setName();
         while(true){
-            System.out.print("Enter nationalId(username):");
-            nationalId = input.nextLine();
+            System.out.println("Enter nationalId(username):");
+            nationalId = utility.setNationalId();
             if( loginService.findNationalId(nationalId) == 1 ) {
                 System.out.println("You enter a wrong national Id!");
             }
             else
                 break;
         }
-        System.out.print("Enter password for " + fullName + " :");
-        password = input.nextLine();
+        System.out.println("Enter password for " + fullName + " :");
+        password = utility.setPassword();
         Clerk newClerk = new Clerk(fullName,nationalId,codeBranch,password,TypeUser.CLERK);
         clerkRepository.add(newClerk);
         loginService.addNewLogin(nationalId,password,TypeUser.CLERK);
