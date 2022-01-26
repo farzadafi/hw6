1476b6e2996fde482f594f49ac758b70ec1e2d4f
farzad <farzadafi40@gmail.com>
2022-01-26 10:48:37 +0330

    add try catch to TransactionRepository

--- Bank/src/main/java/Repository/TransactionRepository.java
+++ Bank/src/main/java/Repository/TransactionRepository.java
@@ -8,60 +8,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionRepository implements Repository<Transaction> {
-    private Connection connection = Singleton.getInstance().getConnection();
+    private Connection connection;
 
-    public TransactionRepository() throws SQLException, ClassNotFoundException {
-        String createTable = "CREATE TABLE IF NOT EXISTS TransactionTable(id serial," +
-                "accountnumber varchar(10)," +
-                "originCardNumber varchar(20),"+
-                "destinationCardNumber varchar(20)," +
-                "amount varchar(50)," +
-                "dateT date," +
-                "timet time," +
-                "typeTransaction varchar(20))";
-        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
-        preparedStatement.execute();
-    }
-
-
-    @Override
-    public void add(Transaction transaction) throws SQLException {
-        String insertTransaction = "INSERT INTO TransactionTable (accountnumber,originCardNumber,destinationCardNumber,amount,dateT,timeT,typeTransaction) VALUES (?, ?, ?, ?, ?, ?, ?) ";
-        PreparedStatement preparedStatement = connection.prepareStatement(insertTransaction);
-        preparedStatement.setString(1,transaction.getAccountNumber());
-        preparedStatement.setString(2,transaction.getOriginCardNumber());
-        preparedStatement.setString(3,transaction.getDestinationCardNumber());
-        preparedStatement.setString(4,transaction.getAmount());
-        preparedStatement.setDate(5,transaction.getDate());
-        preparedStatement.setTime(6,transaction.getTime());
-        preparedStatement.setString(7, String.valueOf(transaction.getTypeTransaction()));
-        preparedStatement.executeUpdate();
-    }
-
-    public List<Transaction> findAllTransaction(String accountNumber, Date date) throws SQLException {
-        String find = "SELECT * FROM TransactionTable WHERE accountnumber = ? AND dateT >= ? ";
-        PreparedStatement preparedStatement = connection.prepareStatement(find);
-        preparedStatement.setString(1,accountNumber);
-        preparedStatement.setDate(2,date);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        List<Transaction> transactionList = new ArrayList<>();
-        while(resultSet.next()){
-            Transaction transaction = new Transaction();
-            transaction.setAccountNumber(resultSet.getString("accountnumber"));
-            transaction.setOriginCardNumber(resultSet.getString("originCardNumber"));
-            transaction.setDestinationCardNumber(resultSet.getString("destinationCardNumber"));
-            transaction.setAmount(resultSet.getString("amount"));
-            transaction.setDate(resultSet.getDate("dateT"));
-            transaction.setTime(resultSet.getTime("timeT"));
-            transaction.setTypeTransaction(TypeTransaction.valueOf(resultSet.getString("typeTransaction")));
-            transactionList.add(transaction);
+    public TransactionRepository() {
+        try {
+            connection = Singleton.getInstance().getConnection();
+            String createTable = "CREATE TABLE IF NOT EXISTS TransactionTable(id serial," +
+                    "accountnumber varchar(10)," +
+                    "originCardNumber varchar(20)," +
+                    "destinationCardNumber varchar(20)," +
+                    "amount varchar(50)," +
+                    "dateT date," +
+                    "timet time," +
+                    "typeTransaction varchar(20))";
+            PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+            preparedStatement.execute();
+        }catch (SQLException | ClassNotFoundException exception){
+            System.out.println(exception.getMessage());
         }
-        return transactionList;
     }
 
 
     @Override
-    public int find(String input) throws SQLException {
+    public void add(Transaction transaction) {
+        try {
+            String insertTransaction = "INSERT INTO TransactionTable (accountnumber,originCardNumber,destinationCardNumber,amount,dateT,timeT,typeTransaction) VALUES (?, ?, ?, ?, ?, ?, ?) ";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertTransaction);
+            preparedStatement.setString(1, transaction.getAccountNumber());
+            preparedStatement.setString(2, transaction.getOriginCardNumber());
+            preparedStatement.setString(3, transaction.getDestinationCardNumber());
+            preparedStatement.setString(4, transaction.getAmount());
+            preparedStatement.setDate(5, transaction.getDate());
+            preparedStatement.setTime(6, transaction.getTime());
+            preparedStatement.setString(7, String.valueOf(transaction.getTypeTransaction()));
+            preparedStatement.executeUpdate();
+        }catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public List<Transaction> findAllTransaction(String accountNumber, Date date) {
+        try {
+            String find = "SELECT * FROM TransactionTable WHERE accountnumber = ? AND dateT >= ? ";
+            PreparedStatement preparedStatement = connection.prepareStatement(find);
+            preparedStatement.setString(1, accountNumber);
+            preparedStatement.setDate(2, date);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Transaction> transactionList = new ArrayList<>();
+            while (resultSet.next()) {
+                Transaction transaction = new Transaction();
+                transaction.setAccountNumber(resultSet.getString("accountnumber"));
+                transaction.setOriginCardNumber(resultSet.getString("originCardNumber"));
+                transaction.setDestinationCardNumber(resultSet.getString("destinationCardNumber"));
+                transaction.setAmount(resultSet.getString("amount"));
+                transaction.setDate(resultSet.getDate("dateT"));
+                transaction.setTime(resultSet.getTime("timeT"));
+                transaction.setTypeTransaction(TypeTransaction.valueOf(resultSet.getString("typeTransaction")));
+                transactionList.add(transaction);
+            }
+            return transactionList;
+        }catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        return null;
+    }
+
+
+    @Override
+    public int find(String input) {
         return 0;
     }
 }
