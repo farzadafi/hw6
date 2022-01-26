f128819ef7f484747f71314b313452f8278d3eb9
farzad <farzadafi40@gmail.com>
2022-01-26 10:06:19 +0330

    add try catch to BankRepository

--- Bank/src/main/java/Repository/BankRepository.java
+++ Bank/src/main/java/Repository/BankRepository.java
@@ -8,33 +8,47 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BankRepository implements Repository<Bank>{
-    private Connection connection = Singleton.getInstance().getConnection();
+    private Connection connection;
 
     //::::>
-    public BankRepository() throws SQLException, ClassNotFoundException {
-        String createTableBank = "CREATE TABLE IF NOT EXISTS Bank(id serial,nameBank varchar(50) PRIMARY KEY)";
-        PreparedStatement preparedStatement = connection.prepareStatement(createTableBank);
-        preparedStatement.execute();
+    public BankRepository() {
+        try {
+            connection = Singleton.getInstance().getConnection();
+            String createTableBank = "CREATE TABLE IF NOT EXISTS Bank(id serial,nameBank varchar(50) PRIMARY KEY)";
+            PreparedStatement preparedStatement = connection.prepareStatement(createTableBank);
+            preparedStatement.execute();
+        }catch (SQLException | ClassNotFoundException exception){
+            System.out.println(exception.getMessage());
+        }
     }
 
     //::::>
     public void add(Bank bank) throws SQLException {
-        String insertBank = "INSERT INTO Bank (nameBank) VALUES (?) ";
-        PreparedStatement preparedStatement = connection.prepareStatement(insertBank);
-        preparedStatement.setString(1,bank.getName());
-        preparedStatement.executeUpdate();
+        try {
+            String insertBank = "INSERT INTO Bank (nameBank) VALUES (?) ";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertBank);
+            preparedStatement.setString(1, bank.getName());
+            preparedStatement.executeUpdate();
+        }catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
     }
 
     //::::>
     public int find(String name) throws SQLException {
-        String find = "SELECT * FROM Bank WHERE nameBank = ? ";
-        PreparedStatement preparedStatement = connection.prepareStatement(find);
-        preparedStatement.setString(1,name);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if(resultSet.next())
-            return 1;
-        else
-            return 0;
+        try {
+            String find = "SELECT * FROM Bank WHERE nameBank = ? ";
+            PreparedStatement preparedStatement = connection.prepareStatement(find);
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next())
+                return 1;
+            else
+                return 0;
+        }catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        return 0;
     }
 
 
