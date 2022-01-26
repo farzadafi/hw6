57970e807971b14ff73c955bc62d4a50d1b35804
farzad <farzadafi40@gmail.com>
2022-01-26 10:39:37 +0330

    add try catch to CustomerRepository

--- Bank/src/main/java/Repository/CustomerRepository.java
+++ Bank/src/main/java/Repository/CustomerRepository.java
@@ -8,44 +8,58 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CustomerRepository implements Repository<Customer> {
-    Connection connection = Singleton.getInstance().getConnection();
+    private Connection connection;
 
-    public CustomerRepository() throws SQLException, ClassNotFoundException {
-        String createTable ="CREATE TABLE IF NOT EXISTS " +
-                "Customer(id serial," +
-                "fullName varchar(50)," +
-                "nationalId varchar(50) PRIMARY KEY ," +
-                "codeBranch varchar(50) REFERENCES BankBranch(codeBranch)," +
-                "password varchar(50))";
-        PreparedStatement preparedStatement = connection.prepareStatement(createTable);
-        preparedStatement.execute();
+    public CustomerRepository() {
+        try {
+            connection = Singleton.getInstance().getConnection();
+            String createTable = "CREATE TABLE IF NOT EXISTS " +
+                    "Customer(id serial," +
+                    "fullName varchar(50)," +
+                    "nationalId varchar(50) PRIMARY KEY ," +
+                    "codeBranch varchar(50) REFERENCES BankBranch(codeBranch)," +
+                    "password varchar(50))";
+            PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+            preparedStatement.execute();
+        }catch (SQLException | ClassNotFoundException exception){
+            System.out.println(exception.getMessage());
+        }
     }
 
     @Override
-    public void add(Customer customer) throws SQLException {
-        String insertCustomer = " INSERT INTO Customer(fullName,nationalId,codeBranch,password) VALUES (?, ?, ?, ?)";
-        PreparedStatement preparedStatement = connection.prepareStatement(insertCustomer);
-        preparedStatement.setString(1,customer.getFullName());
-        preparedStatement.setString(2,customer.getNationalId());
-        preparedStatement.setString(3,customer.getCodeBranch());
-        preparedStatement.setString(4,customer.getPassword());
-        preparedStatement.executeUpdate();
+    public void add(Customer customer) {
+        try {
+            String insertCustomer = " INSERT INTO Customer(fullName,nationalId,codeBranch,password) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertCustomer);
+            preparedStatement.setString(1, customer.getFullName());
+            preparedStatement.setString(2, customer.getNationalId());
+            preparedStatement.setString(3, customer.getCodeBranch());
+            preparedStatement.setString(4, customer.getPassword());
+            preparedStatement.executeUpdate();
+        }catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
     }
 
     @Override
-    public int find(String input) throws SQLException {
+    public int find(String input) {
         return 0;
     }
 
     //::::>
-    public String findName(String nationalId) throws SQLException {
-        String find = "SELECT * FROM Customer WHERE nationalId = ? ";
-        PreparedStatement preparedStatement = connection.prepareStatement(find);
-        preparedStatement.setString(1,nationalId);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if(resultSet.next())
-            return resultSet.getString("fullName");
-        else
-            return "null";
+    public String findName(String nationalId) {
+        try {
+            String find = "SELECT * FROM Customer WHERE nationalId = ? ";
+            PreparedStatement preparedStatement = connection.prepareStatement(find);
+            preparedStatement.setString(1, nationalId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next())
+                return resultSet.getString("fullName");
+            else
+                return "null";
+        }catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        return "null";
     }
 }
