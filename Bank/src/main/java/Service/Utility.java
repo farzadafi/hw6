923730383c3f4a59725ec1a800009091f3cc677a
farzad <farzadafi40@gmail.com>
2022-01-26 13:38:50 +0330

    add setCodeBranch in Utility

--- Bank/src/main/java/Service/Utility.java
+++ Bank/src/main/java/Service/Utility.java
@@ -1,19 +1,21 @@
 package Service;
 
+import Service.Exception.InvalidCodeBranch;
 import Service.Exception.InvalidName;
 
 import java.util.Scanner;
 
 public class Utility {
-    private String firstName;
+    private String firstName,codeBranch;
     private Scanner input = new Scanner(System.in);
     private InvalidName invalidName = new InvalidName();
+    private InvalidCodeBranch invalidCodeBranch = new InvalidCodeBranch();
 
 
 
-    public String setFirstName(){
+    public String setBankName(){
         while(true){
-            System.out.print("Enter first name(just alpha):");
+            System.out.print("Enter name(just alpha):");
             try {
                 firstName = input.nextLine();
                 invalidName.checkName(firstName);
@@ -25,4 +27,18 @@ public class Utility {
         return firstName;
     }
 
-}
+    public String setCodeBranch(){
+        while(true){
+            System.out.print("Enter code Branch(just number):");
+            try {
+                codeBranch = input.nextLine();
+                invalidCodeBranch.codeBranchChecker(codeBranch);
+                break;
+            }catch (InvalidCodeBranch except){
+                System.out.println(except.getMessage());
+            }
+        }
+        return codeBranch;
+    }
+
+}//
